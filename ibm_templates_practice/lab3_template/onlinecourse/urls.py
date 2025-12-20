8b9767078e165febf75212a90dcd28db289e5a3c
rodrigcasio <rod.casio@outlook.com>
2025-12-20 11:52:15 -0600

    `feat:` finish `makemigrations` and `migrate` successfully, Add template and views

--- ibm_templates_practice/lab3_template/onlinecourse/urls.py
+++ ibm_templates_practice/lab3_template/onlinecourse/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 app_name = 'onlinecourse'
 urlpatterns = [
-    # Add path here
+    path(route='', view=views.popular_course_list, name='popular_course_list'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)\
  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- ibm_templates_practice/lab3_template/onlinecourse/views.py
+++ ibm_templates_practice/lab3_template/onlinecourse/views.py
@@ -6,5 +6,17 @@ from django.urls import reverse
 from django.views import generic
 from django.http import Http404
 
-# Create your views here.
+# 1. Add view to get the top 10 popular courses from models
+# - 1.1 Create a context dictionay obj and append course_list into context
+#    IMPORTANT: (next add the route path for popular_course_list view in urls.py)
+#   onlinecourse/urls.py = path(route='', view=views.popular_course_list, name='popular_course_list'),
+#
+
+# 1.
+def popular_course_list(request):
+    context = {}
+    if request.method == 'GET':
+        course_list = Course.objects.order_by('-total_enrollment')[:10]
+        context['course_list'] = course_list
+        return render(request, 'onlinecourse/course_list.html', context) # 1.1
 
