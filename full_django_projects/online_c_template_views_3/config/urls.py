e99a64adb80ea3388023939a965c1d2e7539c0d1
rodrigcasio <rod.casio@outlook.com>
2025-12-20 10:48:32 -0600

    `fix:` ModuleNotFoundError fix within for typo made

--- full_django_projects/online_c_template_views_3/config/urls.py
+++ full_django_projects/online_c_template_views_3/config/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
-from djnago.cong import settings
+from djnago.conf import settings
 
 
 urlpatterns = [
