a5af5f2641334831b758df6fd3b5ef1fc4314ad3
rodrigcasio <rod.casio@outlook.com>
2025-12-19 18:50:56 -0600

    `feat:` Attached postgreSQL database

--- full_django_projects/online_courses_2/online_courses_2/settings.py
+++ full_django_projects/online_courses_2/online_courses_2/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'online_courses_2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',  # own database within Arch database
+        'NAME': 'onlinecourse_db',
+        'USER': 'adminsite_user',
+        'PASSWORD': 'Kumon7700',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
