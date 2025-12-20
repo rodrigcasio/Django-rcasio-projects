40ee490258307bba57bce7b55d031fd92887fbf7
rodrigcasio <rod.casio@outlook.com>
2025-12-20 10:44:44 -0600

    `fix:` Add comma between the first app configuration, previous commit did not make changes

--- full_django_projects/online_c_template_views_3/config/settings.py
+++ full_django_projects/online_c_template_views_3/config/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'onlinecourse.apps.OnlinecourseConfig'
+    'onlinecourse.apps.OnlinecourseConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
