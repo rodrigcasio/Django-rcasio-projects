399d985434d51fca82e290d73546e754f82992c1
rodrigcasio <rod.casio@outlook.com>
2025-12-29 20:07:11 -0600

    `fix` syntax

--- full_django_projects/customer360/customer360/views.py
+++ full_django_projects/customer360/customer360/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from datetime import date, timedelta
 from django.db.models import Count
-from .models import *
+from . models import *
 
 # 1 
 def index(request):
@@ -19,8 +19,7 @@ def create_customer(request):
         customer = Customer.objects.create(name=name, email=email, phone=phone, address=address)
         customer.save()
         msg = "Successfully Saved a Customer"
-        return render(request, "add.html", context={"msg": msg})
-    
+        return render(request, "add.html", context={"msg": msg})   
     return render(request, "add.html")
 
 # 3
@@ -40,14 +39,14 @@ def summary(request):
 
 # 4
 def interact(request, cid):
-    channel = Interaction.CHANNEL_CHOICES
+    channels = Interaction.CHANNEL_CHOICES
     directions = Interaction.DIRECTION_CHOICES
     context = {"channel":channels, "directions":directions}
 
     if request.method == "POST":
         customer = Customer.objects.get(id=cid)
         channel = request.POST["channel"]
-        direction = request.POST["directions"]
+        direction = request.POST["direction"]
         summary = request.POST["summary"]
         interaction = Interaction.objects.create(
                                 customer=customer,
